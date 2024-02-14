acf7b2d6bbd1c5e5d44d405fd2ff83827876b730
kmuramreddy <kmuramreddy@US-GMD7XM3>
2024-02-14 09:48:29 +0530

    Add project files.

---
+++ Assignment/Assignment/PostCommentsAssessment.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Assignment
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PostCommentsAssessment : ContentPage
+    {
+        public PostCommentsAssessment()
+        {
+            InitializeComponent();
+        }
+        private async void OnPostClicked(object sender, EventArgs e)
+        {
+            string firstName = firstNameEntry.Text;
+            string lastName = lastNameEntry.Text;
+            string comments = commentsEntry.Text;
+
+            string message = $"{firstName} {lastName}\n{comments}";
+            await DisplayAlert("Posted Comments", message, "OK");
+
+            ClearEntries();
+        }
+
+        private void OnClearClicked(object sender, EventArgs e)
+        {
+            ClearEntries();
+        }
+
+        private void ClearEntries()
+        {
+            firstNameEntry.Text = string.Empty;
+            lastNameEntry.Text = string.Empty;
+            commentsEntry.Text = string.Empty;
+        }
+    }
+}
